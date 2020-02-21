a0f75c7632265db1ea84f951f2bf283d6e0e9720
liguolin <liguolin@>
2020-02-21 22:44:35 +0800

    init

---
+++ src/main/java/org/bazinga/sheldon/SheldonApplication.java
@@ -0,0 +1,13 @@
+package org.bazinga.sheldon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheldonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SheldonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/bazinga/sheldon/SheldonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bazinga.sheldon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheldonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
